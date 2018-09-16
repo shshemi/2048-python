ee0dc586a297865e5cbe9da3455da118862293f9
Shayan Hashemi <shshemi@gmail.com>
2018-09-16 15:05:00 +0430

    add verbose for background machine player

--- machine_player.py
+++ machine_player.py
@@ -32,7 +32,7 @@ class VisualMachinePlayer(Thread):
 
 class BackgroundMachinePlayer:
 
-    def __init__(self, samples_directory_name, play_algorithm, finish_callback=None, game_id = -1):
+    def __init__(self, samples_directory_name, play_algorithm, finish_callback=None, game_id=-1, verbose=False):
         super().__init__()
         self.samples_directory_name = samples_directory_name
         self.play_function = play_algorithm
@@ -41,6 +41,7 @@ class BackgroundMachinePlayer:
         self.finish_callback = finish_callback
         self.game_id = game_id
         self.play_time = 0
+        self.verbose = verbose
         if game_id != -1:
             print("Game", game_id, "created and ready to be played")
 
@@ -54,6 +55,13 @@ class BackgroundMachinePlayer:
             bonus_score = self.game.move(move)
             if bonus_score != -1:
                 self.history.add_step(matrix, move, bonus_score, self.game.true_score)
+                if self.verbose:
+                    print("Bonus:", bonus_score, "Total Score:", self.game.true_score)
+            else:
+                if self.verbose:
+                    print("Invalid move!")
+                    print("Move:", move)
+                    print(self.game.matrix)
         self.play_time = time.time() - start_time
         if self.game_id != -1:
             print("Game {} finished with {} moves and score {} and play time {}".format(self.game_id, self.game.total_moves, self.game.true_score, self.play_time))
@@ -142,3 +150,52 @@ class AStartAlgorithm:
                 scores.append(bonus + self.predict_score(g.matrix, steps_to_go-1))
         return max(scores) if len(scores) > 0 else 0
 
+
+# ------------------------------- A Start Playing Algorithm -------------------------------
+class TDTreeSearchAlgorithm:
+
+    def __init__(self, incremental_call_count=True):
+        self.call_count = 0
+        self.incremental_call_count = incremental_call_count
+
+    def __call__(self, matrix, *args, **kwargs):
+        if self.incremental_call_count:
+            self.call_count += 1
+        best_move = -1
+        best_score = -1
+        for move in range(4):
+            g = Game(matrix).clone()
+            bonus = g.move(move)
+            if bonus == -1:
+                continue
+            if self.incremental_call_count:
+                predicted = self.predict_score(g.matrix, 3 + int(math.log10(self.call_count)))
+            else:
+                predicted = self.predict_score(g.matrix, 3)
+            if bonus + predicted > best_score:
+                best_score = bonus + predicted
+                best_move = move
+        return best_move
+
+    def predict_score(self, matrix, steps_to_go):
+        game = Game(matrix)
+        if game.is_over():
+            return game.score
+        scores = []
+        for move in range(4):
+            g = game.clone()
+            bonus = g.move(move)
+            if bonus == -1:  # Illegal move
+                continue
+            if g.is_over() or steps_to_go == 0:  # Game is over ever taking a move or we have reached our max dept
+                scores.append(bonus)
+            else:
+                scores.append(bonus + self.predict_score(g.matrix, steps_to_go-1))
+        return np.array(scores).mean()
+
+
+if __name__ == "__main__":
+    player = VisualMachinePlayer("machine_played_samples", AStartAlgorithm(), play_interval=0.25)
+    player.start_playing()
+    # player = BackgroundMachinePlayer("dlru_player_samples", TDTreeSearchAlgorithm())
+    # player.start_playing_as_process()
